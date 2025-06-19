b57240237fed479e0306e91ad25afe2fafd0afc3
LordFarquaad-RJB <ryan.bishop45@gmail.com>
2025-06-19 17:50:48 +0200

    fix interaction aura color after manual triggers

--- Live Files/Live-SYS-TrapSystem.js
+++ Live Files/Live-SYS-TrapSystem.js
@@ -2814,12 +2814,23 @@ const TrapSystem = {
                 TrapSystem.utils.updateTrapUses(trapToken, newUses, trapData.maxUses, true);
             }
 
+            let auraColor;
+            if (newUses > 0) {
+                if (TrapSystem.state.triggersEnabled) {
+                    auraColor = trapData.type === 'interaction'
+                        ? TrapSystem.config.AURA_COLORS.ARMED_INTERACTION
+                        : TrapSystem.config.AURA_COLORS.ARMED;
+                } else {
+                    auraColor = TrapSystem.config.AURA_COLORS.PAUSED;
+                }
+            } else {
+                auraColor = trapData.type === 'interaction'
+                    ? TrapSystem.config.AURA_COLORS.DISARMED_INTERACTION
+                    : TrapSystem.config.AURA_COLORS.DISARMED;
+            }
+
             trapToken.set({
-                aura1_color: newUses > 0 
-                    ? (TrapSystem.state.triggersEnabled 
-                        ? TrapSystem.config.AURA_COLORS.ARMED 
-                        : TrapSystem.config.AURA_COLORS.PAUSED) 
-                    : TrapSystem.config.AURA_COLORS.DISARMED,
+                aura1_color: auraColor,
                 aura1_radius: TrapSystem.utils.calculateDynamicAuraRadius(token),
                 showplayers_aura1: false
             });
